3c7099f2dc67de5afd09b96cec615fa61ecfe148
PTomamselli <pedrotommaselli@gmail.com>
2018-07-11 09:17:55 -0400

    app.js

--- App.js
+++ App.js
@@ -4,17 +4,25 @@ import React from 'react';
 import MenuPage from './src/pages/MenuPage';
 import CategoryItensPage from './src/pages/CategoryItensPage';
 import PrimaryLogin from './src/pages/PrimaryLogin';
-import { Image } from 'react-native';
+import SecondaryLogin from './src/pages/SecondaryLogin';
+import ClientFilterPage from './src/pages/ClientFilterPage';
+
+import { Image, View } from 'react-native';
 
 
 class LogoTitle extends React.Component {
 
   render() {
     return (
+      <View style = {{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',}}>
       <Image
-        source={require('./outline_perm_identity_white_36dp.png')}
-        style={{ width: 70, height: 70 }}
+        source={require('./src/images/outline_perm_identity_white_48.png')}
+        style={{ width: 100, height:100 }}
       />
+      </View>
     );
   }
 }
@@ -28,33 +36,49 @@ class LogoTitle extends React.Component {
 };
 */
 const RootStack = createStackNavigator({
-    'MenuPage': {
-      screen: MenuPage
+    'ClientFilterPage':{
+      screen: ClientFilterPage
     },
-    
+
     'Login': {
         screen: PrimaryLogin,
         navigationOptions:{
           headerTitle: <LogoTitle />,
         headerStyle: {
             backgroundColor: '#e24d4d',
-            height: 250,
-
-
-       },
+            height: 250
+            }
         }
       },
+
+      'SecondaryLogin':{
+        screen: SecondaryLogin,
+        navigationOptions:{
+          headerTitle: <LogoTitle />,
+        headerStyle: {
+            backgroundColor: '#e24d4d',
+            height: 250
+            }
+        }
+      },
+
+      'MenuPage': {
+        screen: MenuPage
+    },
+
 
     'CategoryItensPage': {
       screen: CategoryItensPage
     }
-    }, {
-  navigationOptions: {
+  },{
+
+       navigationOptions: {
        title: "SoftMobile",
        headerTintColor: '#f5f6fa',
        headerStyle: {
             backgroundColor: '#e24d4d',
        },
+
        headerTitleStyle: {
            color: '#f5f6fa',
            fontSize: 25
